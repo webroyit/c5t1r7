fb98d2aed744913fbe92f3a8445bd41568eeaed9
royweb <xytank@gmail.com>
2020-05-18 11:15:49 -0400

    Add a function to generate links

--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -2,9 +2,28 @@
 
 public class Rope : MonoBehaviour
 {
+    // Reference to HingeJoint2D property
+    public HingeJoint2D hook;
+
+    // Reference to the Link prefab
+    public GameObject linkPrefab;
+
+    // Number of links in chain
+    public int links = 7;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateRope();
+    }
+
+    void GenerateRope()
+    {
+        for(int i = 0; i < links; i++)
+        {
+            // Create links
+            Instantiate(linkPrefab, transform);
+        }
     }
 }
