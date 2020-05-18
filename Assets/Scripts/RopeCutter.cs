4e6f217180ae35e31eafc9d26e7e17c11f14c377
royweb <xytank@gmail.com>
2020-05-18 14:23:17 -0400

    Place the ray at the point of the mouse position

--- Assets/Scripts/RopeCutter.cs
+++ Assets/Scripts/RopeCutter.cs
@@ -6,6 +6,13 @@ public class RopeCutter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // 0 is for Left Mouse Button
+        // Listen for the user input
+        if(Input.GetMouseButton(0))
+        {
+            // Place the ray at the point of the mouse position
+            // Vector2.zero to prevent the ray from going anywhere
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Iput.mousePosition), Vector2.zero);
+        }
     }
 }
