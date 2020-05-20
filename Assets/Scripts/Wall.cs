13826e3e43634a912b4a5999ad59a7ffa5695f7a
royweb <xytank@gmail.com>
2020-05-20 15:12:47 -0400

    Add a funciton to restart the level if the Weight hit the wall

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -1,6 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Wall : MonoBehaviour
 {
-    
+    void OnCollisionEnter2D(Collision2D  col)
+    {
+        // Game over if the Weight hitted the ground
+        if(col.collider.tag == "Weight")
+        {
+            // Restart the game
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
 }
