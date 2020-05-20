9d65c50bff7cb635c0432107696f3124edb0992f
royweb <xytank@gmail.com>
2020-05-20 13:03:05 -0400

    Use OnCollisionEnter2D() to check if Weight touch Frog

--- Assets/Scripts/Frog.cs
+++ Assets/Scripts/Frog.cs
@@ -2,5 +2,12 @@
 
 public class Frog : MonoBehaviour
 {
-    
+    void OnCollisionEnter2D(Collision2D  col)
+    {
+        // Player win the game if the Weight touch the frog
+        if(col.collider.tag == "Weight")
+        {
+            Debug.Log("You Win");
+        }
+    }
 }
